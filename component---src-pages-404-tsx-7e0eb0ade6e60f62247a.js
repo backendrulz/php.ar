f30e0dc7570027678e8a3380f410edf5f10043e9
Cristian Ferreyra <cristian@8d2media.com>
2024-03-10 23:50:17 -0300

    Updates

---
+++ component---src-pages-404-tsx-7e0eb0ade6e60f62247a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[453],{731:function(e,n,a){a.r(n);var l=a(6540),t=a(6675);n.default=e=>l.createElement(t.A,null,l.createElement("h1",null,"404 - Página no encontrada"),l.createElement("p",null,"Lamentablemente no pudimos encontrar lo que estabas buscando :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-7e0eb0ade6e60f62247a.js.map
